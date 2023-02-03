9a4bc323ef05d9eec2ed369f8904e0c434813d76
mrunoriginal <87085506+AtlasWiki@users.noreply.github.com>
2023-02-03 13:31:47 -0800

    Added params and values

--- All URL Page Grabber.js
+++ All URL Page Grabber.js
@@ -1,7 +1,6 @@
 // credentials to AtlasWiki
 // paste this in javascript console to use
 
-
 // adds in jquery library and loads it for 4 second before execution
 var jq = document.createElement('script');
 jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js";
@@ -11,7 +10,9 @@ setTimeout(main, 4000)
 function main() {
     // jquery to grab href and src attributes in a list
     href = $('[href]');
-    src= $('[src]');
+    src = $('[src]');
+    names = $('[name]');
+    values = $('[value]');
 
     // grab individual href values from the list
     console.log("\n\n" + `%c
@@ -21,13 +22,12 @@ function main() {
     ███████║██████╔╝█████╗░░█████╗░░
     ██╔══██║██╔══██╗██╔══╝░░██╔══╝░░
     ██║░░██║██║░░██║███████╗██║░░░░░
-    ╚═╝░░╚═╝╚═╝░░╚═╝╚══════╝╚═╝░░░░░`,'color:#649AB3;' + "\n\n\n");
+    ╚═╝░░╚═╝╚═╝░░╚═╝╚══════╝╚═╝░░░░░`, 'color:#649AB3;' + "\n\n\n");
     console.log('%c===========================================================================================================================================================\n\ncurrent website/webpage: ' + document.URL + "\n\n=============================================================================================================================================================\n\n", ' color:#65B383');
     for (let i = 0; i < href.length; i++) {
         console.log('%c[' + (i + 1) + ']', 'background: black; color: #87EEF3; font-weight:bold;', href[i].getAttribute('href'));
-    };
-
-    // grab individual src values from the list
+    }
+    ;// grab individual src values from the list
     console.log("\n\n" + `%c
    
     ░██████╗██████╗░░█████╗░
@@ -37,8 +37,30 @@ function main() {
     ██████╔╝██║░░██║╚█████╔╝
     ╚═════╝░╚═╝░░╚═╝░╚════╝░`, 'color:#C22744' + "\n\n\n")
     console.log('%c===========================================================================================================================================================\n\ncurrent website/webpage: ' + document.URL + "\n\n=============================================================================================================================================================\n\n", ' color:#65B383');
+    // captures params
     for (let i = 0; i < src.length; i++) {
         console.log('%c[' + (i + 1) + ']', 'background: black; color: #87EEF3; font-weight:bold;', src[i].getAttribute('src'));
+    }
+    ;console.log("\n\n" + `%c
+
+██████╗░░█████╗░██████╗░░█████╗░███╗░░░███╗░██████╗
+██╔══██╗██╔══██╗██╔══██╗██╔══██╗████╗░████║██╔════╝
+██████╔╝███████║██████╔╝███████║██╔████╔██║╚█████╗░
+██╔═══╝░██╔══██║██╔══██╗██╔══██║██║╚██╔╝██║░╚═══██╗
+██║░░░░░██║░░██║██║░░██║██║░░██║██║░╚═╝░██║██████╔╝
+╚═╝░░░░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░░░░╚═╝╚═════╝░
+   `, 'color:#846075' + "\n\n\n")
+    console.log('%c===========================================================================================================================================================\n\ncurrent website/webpage: ' + document.URL + "\n\n=============================================================================================================================================================\n\n", ' color:#65B383');
+
+    for (let i = 0; i < names.length; i++) {
+
+        try {
+            var new_values = values[i].getAttribute('value');
+        } catch (error) {
+            new_values = '';
+        }
+
+        console.log('%c[' + (i + 1) + ']', 'background: black; color: #87EEF3; font-weight:bold;', names[i].getAttribute('name') + '=' + new_values);
     };
     jq.remove();
 }
